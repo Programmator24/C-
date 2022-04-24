c7932c86bd73eec5a15f02ec4d4b48c332be2665
Programmator24 <nikita@gmail.com>
2022-04-24 22:01:10 +1000

    condition If-Else

---
+++ Example_005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "merry")
+{
+    Console.WriteLine("WOW, merry");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
