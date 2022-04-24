e8dd0d33de4d023b48ae2d9f0efdfb8f49850d1f
Programmator24 <nikita@gmail.com>
2022-04-24 22:14:44 +1000

    max numer

---
+++ Example_006/Program.cs
@@ -0,0 +1,16 @@
+﻿int numberA = 10;
+int numberB = 9;
+int numberC = 7;
+int numberD = 5;
+int numberE = 4;
+int numberF = 3;
+int max = numberA;
+
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+if(numberD > max) max = numberD;
+if(numberE > max) max = numberE;
+if(numberF > max) max = numberF;
+
+Console.Write("Max= ");
+Console.WriteLine(max);
