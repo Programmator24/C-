5755a8c2617556b0288654cc49165ea67c5f2442
Programmator24 <nikita@gmail.com>
2022-05-21 13:34:47 +1000

    HW N10

---
+++ Example_016HW_N10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10: Напишите программу, которая принимает на
+//вход трёхзначное число и на выходе показывает вторую
+//цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine());
+number = number % 100; // find 2 last numbers
+Console.WriteLine(number / 10);
+
+
