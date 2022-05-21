fee1dc4d535d891587f9736ddeba8bf5b6c4ab3b
Programmator24 <nikita@gmail.com>
2022-05-21 13:35:35 +1000

    HW N13

---
+++ Example_017HW_N13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит
+//третью цифру заданного числа или сообщает, что третьей
+//цифры нет.
+
+Console.WriteLine("Введите число");
+string text = (Console.ReadLine());
+Array.ConvertAll(text.Split(),int.Parse);
+int length = text.Length; 
+if (length < 3)
+{
+   Console.WriteLine("no third number"); 
+}
+else 
+{
+    Console.WriteLine(text[2]); 
+}
+
