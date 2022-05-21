d77b2d0aea0083465ce9af0357237deccaf27fe7
Programmator24 <nikita@gmail.com>
2022-05-21 13:35:57 +1000

    HW N15

---
+++ Example_018HW_N15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 15: Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+//является ли этот день выходным.
+
+Console.WriteLine(" enter day of week, number should be from 1 to 7");
+int day = int.Parse(Console.ReadLine());
+if (day < 6)
+{
+    Console.WriteLine("tooday is work day");
+}
+else if (day < 8)
+{
+    Console.WriteLine("tooday is weekend");
+}
+else 
+Console.WriteLine("incorrect day, number should be from 1 to 7");
