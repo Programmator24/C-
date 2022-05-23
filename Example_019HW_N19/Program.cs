ea7b5057ebb2fca5a4bccd56ee158cc1b33fc156
Programmator24 <nikita@gmail.com>
2022-05-23 22:09:01 +1000

    add new HW

---
+++ Example_019HW_N19/Program.cs
@@ -0,0 +1,50 @@
+﻿//Напишите программу, которая принимает на
+//вход пятизначное число и проверяет, является ли оно
+//палиндромом.
+int[] array = new int[5];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next (1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+void Desision()
+{
+if (array[0] == array[4])
+{
+    if (array[1]  == array[3])
+    {
+        Console.WriteLine("Yes");
+    }
+}
+else 
+{
+    Console.WriteLine("No");
+}
+}
+
+
+
+FillArray(array); 
+PrintArray(array);
+Desision();
+Console.WriteLine();
+
+
---
+++ Example_020HW_N21/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на
+//вход координаты двух точек и находит расстояние между
+//ними в 3D пространстве.
+
+int x1 = 4;
+int y1 = 4;
+int z1 = 8;
+
+int x2 = 4;
+int y2 = -3;
+int z2 = 6;
+
+int AB = x1-x2;
+int BC = y1-y2;
+int AC = z1-z2;
+
+double distance = Math.Sqrt( AB*AB + BC*BC + AC*AC); 
+
+Console.WriteLine(distance);
