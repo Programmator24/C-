9bb9e161de4ea0e11a6251c3d4d1c1928e3bff15
Programmator24 <nikita@gmail.com>
2022-06-04 11:22:17 +1000

    add HW N41,43

---
+++ Example_028HW_N41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+
+Console.WriteLine("Введитие массив чисел через пробел");
+int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Console.WriteLine(String.Join(" ", array));
+
+int sise = array.Length;
+int count = 0;
+for (int n = 0; n < sise; n++)
+{
+    if (array[n] > 0)
+    {
+        count = count + 1;
+    }
+}
+
+Console.WriteLine(count);
+
+
+
---
+++ Example_029HW_N43/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая найдёт точку
+//пересечения двух прямых, заданных уравнениями
+//y = k1 * x + b1, y = k2 * x + b2;
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 =9 -> (-0,5; -0,5)
+Console.WriteLine("Enter b1");
+int b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter k1");
+int k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter b2");
+int b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter k2");
+int k2 = int.Parse(Console.ReadLine());
+
+
+double x = (b2 - b1)/(k2 - k1);
+double y = k2 * x + b2;
+
+Console.WriteLine($"[{x};{y}]");
