c37e2fe5d970bbd2576148f25ee15c657b4fb382
Programmator24 <nikita@gmail.com>
2022-06-12 18:24:14 +1000

    first commit

---
+++ Example_033HW_N64/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+ 
+Console.WriteLine("Enter number");
+int number = int.Parse(Console.ReadLine());
+
+void NaturalNumber(int num)
+{
+    if(num > 0)
+    {
+        Console.Write($"{num} ");
+        num--;
+        NaturalNumber(num);
+        
+    }
+
+}
+NaturalNumber(number);
---
+++ Example_034HW_N66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+//натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Enter number M");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number N");
+int numberN = int.Parse(Console.ReadLine());
+
+
+int SumOfNumbers(int num1, int num2)
+{
+   int s = 0;
+    if (num1 <= num2)
+    s = num1 + SumOfNumbers(num1+1,num2);
+     return s;
+}
+Console.WriteLine(SumOfNumbers(numberM, numberN));
